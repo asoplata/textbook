c35eb08312aa96c63121d5ba4e52d0ade622e1c0
Austin E. Soplata <me@asoplata.com>
2025-10-27 16:29:34 -0400

    ref: build arg str var, use "repo-owner"

--- scripts/get_commit_hash.py
+++ scripts/get_commit_hash.py
@@ -5,6 +5,7 @@ from hnn_core import __version__ as installed_hnn_version
 
 
 def get_commit_hash(build_on_dev_arg):
+    argument_name = "--build-on-dev"
     # get the hash of hnn version installed in the environment
     # this is needed for the checks below
     try:
@@ -57,16 +58,19 @@ def get_commit_hash(build_on_dev_arg):
 
             except Exception as e:
                 raise RuntimeError(
-                    "the --dev-version argument must be specified in the "
-                    'format: --dev-version "your-repository:your-commit-hash" '
-                    "\nE.g., a valid input would be: jonescompneurolab:9e14b99"
+                    f"The {argument_name} argument must be specified in the following"
+                    f'format: {argument_name} "your-repository-owner:your-commit-hash" '
+                    "\nE.g., a valid input would be: asoplata:92b000c, which "
+                    "would correspond to the commit at "
+                    "\nhttps://github.com/asoplata/hnn-core/commit/92b000c597052a661d9e177b8754695446336b96"
                     f"\n\nError message: {e}"
                 )
 
             if commit_hash != installed_hnn_commit:
+                # AES TODO why does this mention the latest? Only equality should matter
                 raise RuntimeError(
-                    "The repository and commit you specified: "
-                    f"\n   Repository: {repo}"
+                    "The repository-owner and commit you specified: "
+                    f"\n   Repository Owner: {repo}"
                     f"\n   Commit: {commit_hash} "
                     "\nDo not match the latest commit on the installed "
                     "version of hnn-core: "
@@ -90,15 +94,16 @@ def get_commit_hash(build_on_dev_arg):
         if installed_hnn_version > latest_stable:
             print(
                 "Warning: your installed version of hnn-core is ahead of the "
-                "current stable version, but you did not use the --build-on-dev "
-                "flag:"
+                f"current stable version, but you did not use the {argument_name}"
+                "argument:"
                 f"\n   Stable version: {latest_stable}"
                 f"\n   Installed version: {installed_hnn_version}"
-                "\nIt is generally advisable to use the --build-on-dev flag "
+                f"\nIt is generally advisable to use the {argument_name} argument "
                 "when generating the textbook on versions of hnn-core that are "
                 "ahead of the current stable version."
             )
         elif installed_hnn_version != latest_stable:
+            # AES need to rewrite this warning
             print(
                 "\nWarning: you are attempting to build the textbook on a "
                 "version of hnn-core that does not match the latest stable version."
@@ -110,7 +115,7 @@ def get_commit_hash(build_on_dev_arg):
                 "\n\nIf your installed version references a particular commit or "
                 "branch (e.g.: hnn-core @ git+https://github.com/jonescompneurolab"
                 "/hnn-core.git@1413550b2c610b700b7bb12ce7e1ae408ef8d4d3),"
-                " we recommend that you use the --build-on-dev flag to specify "
+                f" we recommend that you use the {argument_name} argument to specify "
                 "the version of hnn-core that should be used."
             )
 
